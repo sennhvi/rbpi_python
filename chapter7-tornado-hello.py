4a4558009b6191af826b5802c42f1c4e5f43ea3e
sennhvi <sennhvi@gmail.com>
2018-06-01 02:54:02 +0800

    add c7 template post form

--- chapter7-tornado-hello.py
+++ chapter7-tornado-hello.py
@@ -21,11 +21,21 @@ class HelloHandler(tornado.web.RequestHandler):
                     name=self.name)
 
 
+class FormHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("./chapter7-post-form.html")
+
+    def post(self, *args, **kwargs):
+        # .html.self.get_argument() get user input data
+        self.render("./chapter7-hello-template.html", name=self.get_argument("name"))
+
+
 # create a Tornado web application and set up its options
 if __name__ == "__main__":
     application = tornado.web.Application([
         (r"/", MainHandler),  # a list of tuple defining class processing page.i.e. MainHandler process root page
         (r"/hello/(.*)", HelloHandler),
+        (r"/form/", FormHandler),
     ], )
     application.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
