881b0a62ca043d1e1f0ddb131380c8fcada6c70b
sennhvi <sennhvi@gmail.com>
2018-06-04 02:55:14 +0800

    add c9 cli and mirror implemented by pygame

--- chapter9-mirror.py
+++ chapter9-mirror.py
@@ -12,4 +12,7 @@ cam.start()
 while True:
     image = cam.get_image()
     display.blit(image, (0, 0))
+    # pygame.display.flip() equals to pygame.display.update() when no arguments provided
+    # pygame.display.update(rect_list) only update a rectangle area defined by rect list
+    # rect_list refers to Rect(left, top, width, height)
     pygame.display.flip()
