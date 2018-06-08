f7afa30f216d8b25f4da66af3ffd84375a75e136
sennhvi <sennhvi@gmail.com>
2018-06-09 01:08:41 +0800

    add c12

---
+++ chapter12-debug.py
@@ -0,0 +1,10 @@
+choices = {1: "Start", 2: "Edit", 3: "Quit"}
+for key, value in choices.items():
+    print("Press ", key, " to ", value)
+
+user_input = input("Enter choice: ")
+
+if int(user_input) in choices.keys():
+    print("You chose", choices[int(user_input)])
+else:
+    print("Unknown choice")
