d474c908302c38a6dcb0680a511bdbed64c709c8
sennhvi <sennhvi@gmail.com>
2018-06-23 16:48:45 +0800

    Update chapter5-cli.sh

--- chapter5-cli.sh
+++ chapter5-cli.sh
@@ -1,1 +1,4 @@
- sudo apt-get install libsdl-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsmpeg-dev libportmidi-dev libavformat-dev libswscale-dev mercurial python3-dev
+# No module named rpigl in python.
+# https://github.com/stephanh42/rpigl go to this website and install
+
+sudo apt-get install libsdl-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsmpeg-dev libportmidi-dev libavformat-dev libswscale-dev mercurial python3-dev
